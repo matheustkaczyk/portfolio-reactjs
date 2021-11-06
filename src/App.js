4b5040857bfc4dfe118d5a3e0375810c5594f413
matheustkaczyk <matheus111904@hotmail.com>
2021-11-06 12:52:27 -0300

    Progresso na landingpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import MainPage from '../src/pages/MainPage';
 import ProjectsPage from '../src/pages/projects';
-import Sobre from '../src/pages/sobre';
 
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
@@ -11,7 +10,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={ MainPage }/>
           <Route path="/projects" component={ ProjectsPage }/>
-          <Route path="/about" component={ Sobre }/>
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,24 +9,25 @@ const HeaderComponent = () => {
                 <a href="sobre" className="link-item">Sobre</a>
                 <a href="formacao" className="link-item">Formação</a>
                 <a href="competencias" className="link-item">Competências</a>
+                <a href="contato" className="link-item">Contato</a>
                 <Link className="link-item">Projetos</Link>
             </div>
-            <div className="icons-header">
-                <a className="icon-ref" href="www.linkedin.com/in/matheustkaczykribeiro">
+            {/* <div className="icons-header">
+                <a className="icon-ref" href="https://www.linkedin.com/in/matheustkaczykribeiro">
                     <img
                         className="icon-item" 
                         src="https://cdn-icons-png.flaticon.com/512/1384/1384014.png"
                         alt="icone linkedin"
                     />
                 </a>
-                <a className="icon-ref" href="github.com/matheustkaczyk">
+                <a className="icon-ref" href="https://github.com/matheustkaczyk">
                     <img
                         className="icon-item" 
                         src="https://cdn-icons.flaticon.com/png/512/2175/premium/2175377.png?token=exp=1636212231~hmac=3f69d4ab3097b7767ca8c85ea988af48"
                         alt="icone github"
                     />
                 </a>
-            </div>
+            </div> */}
         </div>
     );
 }
