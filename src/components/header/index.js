487cc97128918d03ffeaf514d6e693864fbfb723
matheustkaczyk <matheus111904@hotmail.com>
2021-11-06 12:14:30 -0300

    configurando header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,11 +5,27 @@ const HeaderComponent = () => {
     return (
         <div className="header-flex">
             <div className="links-div">
-                <Link to="/" className="link-item">Início</Link>
-                <Link className="link-item">Sobre</Link>
-                <Link className="link-item">Formação</Link>
-                <Link className="link-item">Competências</Link>
+                <a href="inicio" className="link-item">Início</a>
+                <a href="sobre" className="link-item">Sobre</a>
+                <a href="formacao" className="link-item">Formação</a>
+                <a href="competencias" className="link-item">Competências</a>
                 <Link className="link-item">Projetos</Link>
+            </div>
+            <div className="icons-header">
+                <a className="icon-ref" href="www.linkedin.com/in/matheustkaczykribeiro">
+                    <img
+                        className="icon-item" 
+                        src="https://cdn-icons-png.flaticon.com/512/1384/1384014.png"
+                        alt="icone linkedin"
+                    />
+                </a>
+                <a className="icon-ref" href="github.com/matheustkaczyk">
+                    <img
+                        className="icon-item" 
+                        src="https://cdn-icons.flaticon.com/png/512/2175/premium/2175377.png?token=exp=1636212231~hmac=3f69d4ab3097b7767ca8c85ea988af48"
+                        alt="icone github"
+                    />
+                </a>
             </div>
         </div>
     );
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -4,7 +4,7 @@ const Main = () => {
     return (
     <div className="hero-div" >
         <div className="info-div">
-            <h2>Seja bem-vindo!</h2>
+            <h2 id="inicio" className="hero-welcome">Seja bem-vindo!</h2>
             <h1 className="hero-title"> Matheus Tkaczyk Ribeiro </h1>
             <h2 className="hero-subtitle">Desenvolvedor Web Jr.</h2>
         </div>
