d124f2178615c205059ad9ea4f0e152c44fe24ff
matheustkaczyk <matheus111904@hotmail.com>
2021-11-28 14:15:59 -0300

    INB4 reformulacao

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import react from '../../images/react.png';
+import html from '../../images/html.png';
+import css from '../../images/css-3.png';
+import js from '../../images/js.png';
+import redux from '../../images/redu.png';
 
 const Skills = () => {
     return (
         <div id="competencias" className="skills-session">
-            <div classNam="skills-section">
-                <h2>Como posso ajudar?</h2>
+            <div className="skills-section">
+                <h2 className="skills-hero-text">Como posso ajudar?</h2>
                 <div className="skills-images">
-                    <p>imagens</p>
+                    <div className="skills-front">
+                        <img className="skills-item" src={ react } alt="react-icon"/>
+                        <img className="skills-item" src={ html } alt="html-icon"/>
+                        <img className="skills-item" src={ css } alt="css-icon"/>
+                        <img className="skills-item" src={ js } alt="js-icon"/>
+                        <img className="skills-item" src={ redux } alt="redux-icon"/>
+                    </div>
+                    <div className="skills=back">
+                        <img className="skills-item" alt="node-icon"/>
+                        <img className="skills-item" alt="express-icon"/>
+                        <img className="skills-item" alt="mysql-icon"/>
+                        <img className="skills-item" alt="mongodb-icon"/>
+                    </div>
                 </div>
             </div>
         </div>
